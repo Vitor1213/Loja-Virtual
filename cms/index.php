b781efcc926e96afe9b9bdc39dc19fcc6db24fc3
Vitor Hugo <vitor1213.dev@gmail.com>
2019-07-11 21:36:05 -0300

    Adicionando novos conteúdos

--- cms/index.php
+++ cms/index.php
@@ -1,52 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>Loja Virtual</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="css/shop-homepage.css" rel="stylesheet">
-
-</head>
-
-<body>
-
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container">
-      <a class="navbar-brand" href="#">Loja Virtual</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Página Inicial
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Informações</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Serviço ao cliente</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Minha conta</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+<?php require_once('inc/header.php'); ?>
 
   <!-- Page Content -->
   <div class="container">
@@ -219,19 +171,4 @@
   </div>
   <!-- /.container -->
 
-  <!-- Footer -->
-  
-  <footer class="py-5 bg-dark">
-    <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
-    </div>
-    <!-- /.container -->
-  </footer>
-
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-</body>
-
-</html>
+<?php require_once('inc/footer.php'); ?>
