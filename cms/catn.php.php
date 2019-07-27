4caadefec29302ed74d8f1af545c69760833afd0
mricarado <rick100mario@hotmail.com>
2019-07-27 11:45:06 -0300

    AtualizaçãoSite-Mario/07/2019/27

---
+++ cms/catn.php.php
@@ -0,0 +1,112 @@
+<?php require_once('inc/header.php'); ?>
+
+  <!-- Page Content -->
+  <div class="container">
+  
+			  <h5>Destaques</h5>
+	  <hr>
+
+	
+    <div class="row">
+
+      <div class="col-lg-3">
+	  
+			  <h5>Destaques</h5>
+	  <hr>
+
+        <a href="catps.php"><h4 class="my-4">Playstation</h4></a>
+        <div class="list-group">
+          <a href="consoleps.php" class="list-group-item">Consoles</a>
+          <a href="jogosps.php" class="list-group-item">Jogos</a>
+          <a href="acessoriosps.php" class="list-group-item">Acessórios</a>
+        </div>
+
+         <a href="catn.php"><h4 class="my-4">Nitendo</h4></a>
+        <div class="list-group">
+          <a href="consolen.php" class="list-group-item">Consoles</a>
+          <a href="jogosn.php" class="list-group-item">Jogos</a>
+          <a href="acessoriosn.php" class="list-group-item">Acessórios</a>
+        </div>
+	  
+        <a href="catx.php"><h4 class="my-4">Xbox</h4></a>
+        <div class="list-group">
+          <a href="consolex.php" class="list-group-item">Consoles</a>
+          <a href="jogosx.php" class="list-group-item">Jogos</a>
+          <a href="acessoriosx.php" class="list-group-item">Acessórios</a>
+        </div>
+		
+		</div>
+    
+      <!-- /.col-lg-3 -->
+
+      <div class="col-lg-9">
+
+		
+			  <h5>Destaques</h5>
+	  <hr>
+
+        <div class="row">
+
+
+    <!-- Three columns of text below the carousel -->
+    <div class="row">
+      <div class="col-lg-3 text-center">
+	  <a href="consoleps.php">
+	  <div class=""> <img src="img/switch-175x175.png" alt="" class="img-circle topmar"> 
+	  <h2>Consoles</h2>
+	 </div>
+	 </a>
+	 </div>
+     
+ 
+      </div><!-- /.col-lg-3 -->
+      <div class="col-lg-3 text-center">
+	   <a href="jogosps.php">
+         <div class=""> <img src="img/cd-175x175.jpg" alt="" class="img-circle topmar"> 
+	  <h2>Jogos</h2> 
+      </div><!-- /.col-lg-4 -->
+	  </a>
+	  </div>
+	  
+      <div class="col-lg-3 text-center">
+	   <a href="acessoriosps.php">
+         <div class=" "> <img src="img/cont-switch-175x175.png" alt="" class="img-circle topmar"> 
+	  <h2>Acessórios</h2>
+      </div><!-- /.col-lg-4 -->
+	  </a>
+    </div><!-- /.row -->
+
+
+       
+      
+        <!-- /.row -->
+
+      </div>
+
+
+        </div>
+        <!-- /.row -->
+
+      </div>
+      <!-- /.col-lg-9 -->
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+  <div class="container bg-2 text-center">
+    <div class="row">
+	<div class="col-4">
+	</div>
+	<div class="col-4">
+	</div>
+	<div class="col-4">
+	</div>
+  
+  </div>
+
+  
+  </div>
+
+<?php require_once('inc/footer.php'); ?>
